6e42d867a75a3582dfe999e529c986e1e4dd2db1
yancy0109 <1156729237@qq.com>
2023-04-03 18:37:13 +0800

    module step14 update

--- spring-step14/src/main/java/com/yancy/springframework/beans/factory/PropertyPlaceholderConfigurer.java
+++ spring-step14/src/main/java/com/yancy/springframework/beans/factory/PropertyPlaceholderConfigurer.java
@@ -7,6 +7,7 @@ import com.yancy.springframework.beans.core.io.DefaultResourceLoader;
 import com.yancy.springframework.beans.core.io.Resource;
 import com.yancy.springframework.beans.factory.config.BeanDefinition;
 import com.yancy.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import com.yancy.springframework.utils.StringValueResolver;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -51,6 +52,7 @@ public class PropertyPlaceholderConfigurer implements BeanFactoryPostProcessor {
             Properties properties = new Properties();
             properties.load(resource.getInputStream());
 
+            // 占位符替换属性值，设置属性值
             String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
             for (String beanName : beanDefinitionNames) {
                 BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanName);
@@ -59,24 +61,52 @@ public class PropertyPlaceholderConfigurer implements BeanFactoryPostProcessor {
                 for (PropertyValue propertyValue : propertyValues.getPropertyValues()) {
                     Object value = propertyValue.getValue();
                     if (!(value instanceof String)) continue;
-                    String strVal = (String) value;
-                    StringBuilder buffer = new StringBuilder(strVal);
-                    int startIdx = strVal.indexOf(DEFAULT_PLACEHOLDER_PREFIX);
-                    int stopIdx = strVal.indexOf(DEFAULT_PLACEHOLDER_SUFFIX);
-                    // 如果为 占位符
-                    if (startIdx != -1 && stopIdx != -1 && startIdx < stopIdx) {
-                        String propKey = strVal.substring(startIdx + 2, stopIdx);
-                        String propVal = properties.getProperty(propKey);
-                        buffer.replace(startIdx, stopIdx + 1, propVal);
-                        // propertyValue 替换
-                        propertyValues.addPropertyValue(
-                                new PropertyValue(propertyValue.getName(), buffer.toString())
-                        );
-                    }
+                    value = this.resolvePlaceholder((String) value, properties);
+                    // propertyValue 替换
+                    propertyValues.addPropertyValue(
+                            new PropertyValue(propertyValue.getName(), value)
+                    );
                 }
+                // 向容器中添加字符串解析器，供解析@Value注解使用
+                PlaceholderResolvingStringValueResolver valueResolver = new PlaceholderResolvingStringValueResolver(properties);
+//                beanFactory.addEmbeddedValueResolver(valueResolver);
             }
         } catch (IOException e) {
             throw new BeansException("Could not load properties");
+        }
+    }
+
+    /**
+     * 根据 value 进行解析，如果为占位符，则返回 Properties 中对应的属性值，否则返回原值
+     * @param value
+     * @param properties
+     * @return realValue
+     */
+    private String resolvePlaceholder(String value, Properties properties) {
+        String strVal = value;
+        StringBuilder buffer = new StringBuilder(strVal);
+        int startIdx = strVal.indexOf(DEFAULT_PLACEHOLDER_PREFIX);
+        int stopIdx = strVal.indexOf(DEFAULT_PLACEHOLDER_SUFFIX);
+        // 如果为 占位符
+        if (startIdx != -1 && stopIdx != -1 && startIdx < stopIdx) {
+            String propKey = strVal.substring(startIdx + 2, stopIdx);
+            String propVal = properties.getProperty(propKey);
+            buffer.replace(startIdx, stopIdx + 1, propVal);
+        }
+        return buffer.toString();
+    }
+
+    private class PlaceholderResolvingStringValueResolver implements StringValueResolver {
+
+        private final Properties properties;
+
+        public PlaceholderResolvingStringValueResolver(Properties properties) {
+            this.properties = properties;
+        }
+
+        @Override
+        public String resolveStringValue(String strVal) {
+            return PropertyPlaceholderConfigurer.this.resolvePlaceholder(strVal, properties);
         }
     }
 }
