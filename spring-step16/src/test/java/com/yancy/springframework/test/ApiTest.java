adc5f2e9ab405b96aeebd24dcf5c6d7818188777
yancy0109 <1156729237@qq.com>
2023-04-05 23:07:14 +0800

    module step16 test...

---
+++ spring-step16/src/test/java/com/yancy/springframework/test/ApiTest.java
@@ -0,0 +1,25 @@
+package com.yancy.springframework.test;
+
+import com.yancy.springframework.context.support.ClassPathXmlApplicationContext;
+import com.yancy.springframework.test.bean.TestService2;
+import org.junit.Test;
+
+/**
+ * @author yancy0109
+ */
+public class ApiTest {
+
+    @Test
+    public void test_reRely() {
+        // 修改前, 循环依赖会导致发生 StackOverflowError
+        // 因为会导致不停递归调用 getBean, 不停创建新对象, 但是新对象也需要 getBean 获取 依赖于它自己的新对象
+        try {
+            ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+              "classpath:spring.xml"
+            );
+                applicationContext.getBean("userService", TestService2.class);
+        } catch (StackOverflowError e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ spring-step16/src/test/java/com/yancy/springframework/test/bean/TestService1.java
@@ -0,0 +1,10 @@
+package com.yancy.springframework.test.bean;
+
+/**
+ * @author yancy0109
+ */
+public class TestService1 {
+
+    private TestService2 testService2;
+
+}
